ba3046b5e8ce65b09105b7cc7a0126b0df8dc916
tonipelovski <tonipelovski@gmail.com>
2020-01-17 13:30:01 +0200

    showing coordinates on map with google api

--- app/src/main/java/com/example/theking/securityapp/MainActivity.java
+++ app/src/main/java/com/example/theking/securityapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.theking.securityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent gpsActivity = new Intent(MainActivity.this, GPSActivity.class);
+        MainActivity.this.startActivity(gpsActivity);
     }
 }
